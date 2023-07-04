23d7fe9f3a00a18047aaf41bdca9264193f686f7
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2023-07-04 07:47:54 +0530

    update `python3.5` to `python3.8`

--- src/server/JasmineGraphInstanceService.cpp
+++ src/server/JasmineGraphInstanceService.cpp
@@ -2016,7 +2016,7 @@ void *instanceservicesession(void *dummyPt) {
             std::transform(predictargs.begin(), predictargs.end(), std::back_inserter(predict_agrs_vector), converter);
 
             std::string path = "cd " + utils.getJasmineGraphProperty("org.jasminegraph.graphsage") + " && ";
-            std::string command = path + "python3.5 predict.py ";
+            std::string command = path + "python3. predict.py ";
 
             int argc = predictargs.size();
             for (int i = 0; i < argc; ++i) {
